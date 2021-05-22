1ba2030a390ffe063bba19ac691e81ed4457ed38
sedighemohammadkhani <semohammadkhani@gmail.com>
2021-05-22 13:18:20 +0430

    time log

--- src/app/calender/components/task-log/task-log.component.ts
+++ src/app/calender/components/task-log/task-log.component.ts
@@ -11,6 +11,7 @@ export class TaskLogComponent implements OnInit {
   logForm: FormGroup;
   @Output() Done = new EventEmitter();
   logs = [];
+  hours=[1,2,3,4,5,6,7,8,9]
 
   constructor(private eventService: EventService) {}
 
